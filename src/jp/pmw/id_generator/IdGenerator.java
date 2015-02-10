3e8935b36f0d873ffb5e5294522516dfb2a4ec6c
shota <si@primary-meta-works.com>
2015-02-10 11:07:23 +0900

    v2.1.0
    大学文科省番号を含む、計12IDを生成可能にしました.
    まだテストは行っていません.

--- src/jp/pmw/id_generator/IdGenerator.java
+++ src/jp/pmw/id_generator/IdGenerator.java
@@ -33,6 +33,8 @@ public class IdGenerator{
 	private static final String REVISTION_DIGIT_STAFF_ID = "%04d";
 	//教室IDの補正桁数
 	private static final String REVISTION_DIGIT_ROOM_ID = "%04d";
+	//教室マップセルの補正桁数
+	private static final String REVISTION_DIGIT_ROOM_MAP = "%02d";
 	//時間IDの桁補正
 	private static final String REVISTION_DIGIT_TIMETABLE_ID = "%02d";
 	//科目IDの補正桁数
@@ -41,6 +43,8 @@ public class IdGenerator{
 	private static final String REVISTION_DIGIT_STUDENT_ID = "%04d";
 	//教員IDの登録順桁数
 	private static final String REVISTION_DIGIT_FACULTY_ID = "%04d";
+	//日付の補正桁数
+	private static final String REVISTION_DIGIT_DATE_ID = "%02d";
 
 	//文科省大学番号
 	private static String mextNumber;
@@ -102,6 +106,7 @@ public class IdGenerator{
 	 * 学科IDを生成する
 	 *
 	 * 学科ID char(17)
+	 * ・DEPUT
 	 * ・文科省大学番号(6)
 	 * ・教育機関区分(1)
 	 * ・大分類(2)
@@ -118,28 +123,36 @@ public class IdGenerator{
 		return IDENTIFY_DEPT_ID + getMextNumber() + eduationSection + largeCategory + middleCategory + smallCategory;
 	}
 
-
-
-
-
 	/**
 	 * createdate : 2015年1月23日
-	 * generateStudentIdメソッド
-	 * 学生IDを生成する
-	 * @parm year 登録年度(2桁)
-	 * @parm index 番号
-	 * @return 学生ID
+	 * lastchange : 2015年2月10日
+	 * generateRoomIdメソッド
+	 * 教室IDを生成する.
+	 *
+	 * 教室ID char (14)
+	 * ・ROOM
+	 * ・文科省大学番号(6)
+	 * ・登録順(4)
+	 * @parm number 登録順番号
+	 * @return 教室ID
 	 **/
-	public static String generateStudentId(String year,int index){
-		String rivStudent = revistion(REVISTION_DIGIT_STUDENT_ID,index);
-		String addYearStudent = year + rivStudent;
-		return createId(addYearStudent);
+	public static String generateRoomId(int number){
+		String rivRoomId = revistion(REVISTION_DIGIT_ROOM_ID,number);
+		return IDENTIFY_ROOM_ID + getMextNumber() + rivRoomId;
 	}
 
+
 	/**
 	 * createdate : 2015年1月23日
+	 * lastchange : 2015年2月10日
 	 * generateSeatIdメソッド
 	 * 座席IDを生成する
+	 *
+	 * 座席ID(20)
+	 * ・教室ID(14)
+	 * ・教室マップセル行(2)
+	 * ・教室マップセル列(2)
+	 *  ・SE (2)識別英字
 	 * @parm roomId 教室ID
 	 * @parm roomMapCellRow 教室マップ行番号
 	 * @parm roomMapCellColumn 教室マップ列番号
@@ -148,74 +161,136 @@ public class IdGenerator{
 	public static String generateSeatId(String roomId,int roomMapCellRow,int roomMapCellColumn){
 		String rivCellRow = revistion(REVISTION_DIGIT_ROOM_MAP,roomMapCellRow);
 		String rivCellColumn = revistion(REVISTION_DIGIT_ROOM_MAP,roomMapCellColumn);
-		String seatId = roomId + rivCellRow + rivCellColumn;
-		return seatId;
+		return roomId + rivCellRow + rivCellColumn + IDENTIFY_SEAT_ID;
 	}
 
-	/**
-	 * createdate : 2015年1月23日
-	 * generateRoomIdメソッド
-	 * 教室IDを生成する.
-	 * @parm index 登録順番号
-	 * @return 教室ID
-	 **/
-	public static String generateRoomId(int index){
-		String rivRoomId = revistion(REVISTION_DIGIT_ROOM_ID,index);
-		return createId(rivRoomId);
-	}
+
 
 	/**
-	 * createdate : 2015年1月23日
-	 * generateBuildingIdメソッド
-	 * 建物IDを生成する.
-	 * @parm index 登録順番号
-	 * @return 建物ID
-	 **/
-	public static String generateBuildingId(int index){
-		String rivBuilding = revistion(REVISTION_DIGIT_BUILDING_ID,index);
-		return createId(rivBuilding);
-	}
-
-	/**
-	 * createdate : 2015年1月23日
-	 * generateCampuIdメソッド
-	 * キャンパスIDを生成する.
-	 * @parm index 登録順番号
-	 * @return キャンパスID
-	 **/
-	public static String generateCampuId(int index){
-		String rivCampus = revistion(REVISITON_DIGIT_CAMPUS_ID,index);
-		String campusId = mextNumber + rivCampus;
-		setCampusId(campusId);
-		return campusId;
-	}
-
-	/**
-	 * createdate : 2015年1月23日
-	 * generateTimetableIdメソッド
-	 * 時間帯IDを生成する.
+	 * createdate : 2015年2月10日
+	 * generateAcademicCalendarIdメソッド
+	 * 学年歴IDを生成する
+	 *
+	 * 学年歴ID  char (26)
+	 * ・学科ID(17)
+	 * ・年(2)
+	 * ・月(2)
+	 * ・日(2)
+	 * ・ AC(2) 学年歴識別英字
+	 *
 	 * @parm year 登録年度(2桁)
-	 * @parm index 登録順番号
-	 * @return 時間帯ID
+	 * @parm index 番号
+	 * @return 学生ID
 	 **/
-	public static String generateTimetableId(String year,int index){
-		String rivTimetable = revistion(REVISTION_DIGIT_TIMETABLE_ID,index);
+	public static String generateAcademicCalendarId(String deptId,String year,String month,String day){
+		return deptId + revisionDate(year) + revisionDate(month) + revisionDate(day) + IDENTIFY_ACADEMIC_CALENDAR_ID;
+
+	}
+
+	/**
+	 * createdate : 2015年2月10日
+	 * revisionDateメソッド
+	 * 日付に関する文字列を2桁に補正する.
+	 * @return 日付
+	 **/
+	private static String revisionDate(String str){
+		if(str.length() == 1){
+			str = "0" + str;
+		}
+		return str;
+	}
+
+
+	/**
+	 * createdate : 2015年1月23日
+	 * lastchange : 2015年2月10日
+	 * generateTimetableIdメソッド
+	 * 時限IDを生成する.
+	 *
+	 * 時限ID char (25)
+	 * ・学科ID(17)
+	 * ・登録年度(2)
+	 * ・登録順(4)
+	 * ・SU(2)科目識別英字
+	 *
+	 * @parm deptId 学科ID
+	 * @parm year 登録年度(2桁)
+	 * @parm number 登録順番号
+	 * @return 時限ID
+	 **/
+	public static String generateTimetableId(String deptId,String year,int number){
+		String rivTimetable = revistion(REVISTION_DIGIT_TIMETABLE_ID,number);
 		String addYearTimetable = year + rivTimetable;
-		return createId(addYearTimetable);
+		return deptId + addYearTimetable + IDENTIFY_TIMETABLE_ID;
 	}
 
 	/**
 	 * createdate : 2015年1月23日
+	 * lastchange : 2015年2月10日
 	 * generateSubjectIdメソッド
 	 * 科目IDを生成する.
+	 *
+	 * 科目ID(25)
+	 * ・学科ID(17)
+	 * ・登録年度(2)
+	 * ・登録順(4)
+	 * ・SU(2) 科目ID識別英字
+	 *
+	 * @parm deptId 学科ID
 	 * @parm year 登録年度(2桁)
-	 * @parm index 登録順番号
+	 * @parm number 登録順番号
 	 * @return 科目ID
 	 **/
-	public static String generateSubjectId(String year,int index){
-		String rivSubject = revistion(REVISTION_DIGIT_SUBJECT_ID,index);
+	public static String generateSubjectId(String deptId, String year, int number){
+		String rivSubject = revistion(REVISTION_DIGIT_SUBJECT_ID,number);
 		String addYearSubject = year + rivSubject;
-		return createId(addYearSubject);
+		return deptId + addYearSubject + IDENTIFY_SUBJECT_ID;
+	}
+
+	/**
+	 * createdate : 2015年1月23日
+	 * lastchange : 2015年2月10日
+	 * generateStudentIdメソッド
+	 * 学生IDを生成する
+	 *
+	 * 学生ID(25)
+	 * ・学科ID(17)
+	 * ・登録年度(2)
+	 * ・登録順(4)
+	 * ・ST(2)学生ID識別英字
+	 *
+	 * @param deptId 学科ID
+	 * @parm year 登録年度(2桁)
+	 * @parm number 番号
+	 * @return 学生ID
+	 **/
+	public static String generateStudentId(String deptId,String year,int number){
+		String rivStudent = revistion(REVISTION_DIGIT_STUDENT_ID,number);
+		String addYearStudent = year + rivStudent;
+		return deptId + addYearStudent + IDENTIFY_STUDENT_ID;
+	}
+
+
+	/**
+	 * createdate : 2015年2月10日
+	 * generateFacultyIdメソッド
+	 * 教員IDを生成する
+	 *
+	 * 教員ID(25)
+	 * ・学科ID(17)
+	 * ・登録年度(2)
+	 * ・登録順(4)
+	 * ・FA(2)教員ID識別英字
+	 *
+	 * @param deptId 学科ID
+	 * @parm year 登録年度(2桁)
+	 * @parm number 番号
+	 * @return 学生ID
+	 **/
+	public static String generateFacultyId(String deptId,String year,int number){
+		String rivFaculty = revistion(REVISTION_DIGIT_FACULTY_ID,number);
+		String addYearFaculty = year + rivFaculty;
+		return deptId + addYearFaculty + IDENTIFY_FACULTY_ID;
 	}
 
 	/**
